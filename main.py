e7865bbbaeff62e356a6183aac17b5a20ab65dee
sanketbalbudhe <sanketbalbudhe@gmail.com>
2026-08-22 12:02:22 +0530

    Define add_numbers and greet functions
    Fix the add_numbers function to include 'c' as a parameter.

---
+++ main.py
@@ -0,0 +1,9 @@
+def add_numbers(a, b):
+    return a + b + c
+
+def greet(name):
+    return f"Hello, {name}!"
+
+if __name__ == "__main__":
+    print(add_numbers(10, 20))
+    print(greet("Students"))
